6fe5e4fea568a087663e1a73ef39d29593de8402
Mustafamm <mustafamukhlis@outlook.com>
2020-02-10 17:30:44 -0700

    First Trial

---
+++ src/First_Eclipse.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class First_Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner newsca = new Scanner(System.in);  // Create a Scanner object
+		    System.out.println("Enter username");
+
+		    String userName = newsca.nextLine();  // Read user input
+		    System.out.println("Username is: " + userName);  // Output user input
+		    newsca.close();
+	}
+
+}
+	
