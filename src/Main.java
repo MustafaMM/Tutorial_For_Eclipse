537644ca04c7fbdf73f5afb756f792d76b5ab1d4
FabianP <fabianaryo@gmail.com>
2020-02-10 17:53:32 -0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hey");
+		Scanner in = new Scanner(System.in); 
+		String word = in.nextLine();
+		System.out.println(word);
+	}
+
+}
